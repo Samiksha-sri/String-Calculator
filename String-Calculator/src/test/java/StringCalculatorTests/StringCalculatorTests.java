97281ee4e14c962fd78c02f84e4e4f6b2971ed28
Samiksha <srivastavasamiksha97@gmail.com>
2023-05-26 11:28:11 +0530

    Added project for String Calculator

---
+++ String-Calculator/src/test/java/StringCalculatorTests/StringCalculatorTests.java
@@ -0,0 +1,17 @@
+package StringCalculatorTests;
+
+import org.junit.Test;
+
+public class StringCalculatorTests {
+
+    public static  void main(String args[]){
+
+    }
+
+    @Test
+    public void nothing(){
+
+        //Test nothing
+
+    }
+}
